0b358c85b61f77c00e5e42db4f832b31ff05a3d4
Kovalenko Yuriy <yura.kovalenko.1996.29@gmail.com>
2019-01-09 18:11:55 +0200

    Add shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { NavComponent } from './components/nav/nav.component';
+import {AppComponent} from './app.component';
+import {NavComponent} from './components/nav/nav.component';
+import {SimpleComponent} from './components/sliders/simple/simple.component';
+import {SharedModule} from "./shared/shared.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    SimpleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/shared/components/svg/svg.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-svg',
+  templateUrl: './svg.component.html',
+  styleUrls: ['./svg.component.scss']
+})
+export class SvgComponent {
+  @Input() name: String;
+
+  constructor() {}
+
+  get absUrl() {
+    return window.location.href;
+  }
+}
